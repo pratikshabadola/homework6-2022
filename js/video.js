1db3d6ebd97303cd4967556ee736bdf517139b46
pratikshabadola <97632623+pratikshabadola@users.noreply.github.com>
2022-04-11 21:02:12 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,11 +1,75 @@
-var video;
+var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+	console.log("Good job opening the window");
+	video.autoplay = false;
+	video.loop = false;
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow down video");
+	video.playbackRate *= 0.95;
+	console.log("Speed is ", video.playbackRate)
+
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Speed up video");
+	video.playbackRate /= 0.95;
+	console.log("Speed is ", video.playbackRate);
+});
+
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Current location is ", video.currentTime);
+	console.log("Skip ahead");
+	video.currentTime += 15;
+	if (video.currentTime >= video.duration) {
+		video.currentTime = 0;
+		console.log("Going back to the beginning");
+	}
+	console.log("New location is ", video.currentTime);
+
+});
+
+document.querySelector("#mute").addEventListener("click", function() {
+	if (document.querySelector("#mute").innerHTML == "Mute") {
+		document.querySelector("#mute").innerHTML = "Unmute"
+		video.muted = true;
+		console.log("Muted");
+
+	}
+	else {
+		document.querySelector("#mute").innerHTML = "Mute"
+		video.muted = false;
+		console.log("Unmuted");
+	}
+});
+
+document.querySelector("#slider").addEventListener("change", function() {		
+	console.log("Change volume level");
+	video.volume = document.getElementById("slider").value / 100;
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {		
+	console.log("Change to vintage");
+	video.classList.add('oldSchool');
+
+});
+
+document.querySelector("#orig").addEventListener("click", function() {		
+	console.log("Change to original");
+	video.classList.remove('oldSchool');
+});
